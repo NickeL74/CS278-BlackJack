71433cbd76be89c8161a5645eca9e26ad7687b3a
NickeL74 <nicholaslittle20@my.whitworth.edu>
2017-12-05 14:06:12 -0800

    Summing hand and end game logic

--- BlackJack/Main.asm
+++ BlackJack/Main.asm
@@ -7,17 +7,24 @@ INCLUDE Irvine32.inc
 ;player hand values
 playerHand BYTE 14 DUP(?)
 cardsInPlayerHand BYTE 0
-playerHandIndicator BYTE "------Your Hand-----", 0ah, 0h
+sumOfPlayerHand DWORD 0
+stayValue BYTE 0
+playerHandIndicator BYTE "------Your Hand-----", 0ah, "sum:", 0h
 
 ;dealer hand values
 dealerHand BYTE 14 DUP(?), 1
 cardsInDealerHand BYTE 0
+sumOfDealerHand DWORD 0
 dealerHandIndicator BYTE "------Dealer's Hand------", 0ah, 0h
+
+;End Conditions
+winMessage BYTE "You WIN!!", 0ah, 0h
+loseMessage BYTE "You Lost....", 0ah, "Better luck next time.", 0ah, 0h
 
 ;User input values
 whatToDo BYTE "Would you like to hit or stay?", 0ah, "Type 'hit' or 'stay' then press enter to submit.", 0ah, 0h
 input BYTE 5 DUP(?)
-hitTest BYTE "hit"
+
 
 
 ;Cards displayed
@@ -123,6 +130,11 @@ King BYTE 0dah, 0c4h, 0c4h, 0c4h, 0c4h, 0c4h, 0c4h, 0c4h, 0c4h, 0c4h, 0c4h, 0bfh
 
 .code
 displayPlayerHand proc
+	mov edx, OFFSET playerHandIndicator
+	call WriteString ;Title hand
+	mov eax, sumOfPlayerHand
+	call WriteInt
+	call CrLf
 	mov ecx, LENGTHOF playerHand ;set loop number for number of cards in hand
 	dec ecx ;decrease ecx bacause array starts with 0 not 1
 	L1: ;loop through all cards
@@ -264,6 +276,11 @@ displayPlayerHand proc
 displayPlayerHand endp
 
 displayDealerHand proc
+	mov edx, OFFSET dealerHandIndicator
+	call WriteString ;Title hand
+	mov eax, sumOfDealerHand
+	call WriteInt
+	call CrLf
 	mov ecx, LENGTHOF dealerHand ;set loop number for number of cards in hand
 	dec ecx ;decrease ecx bacause array starts with 0 not 1
 	L1: ;loop through all cards
@@ -414,14 +431,29 @@ displayDealerHand proc
 displayDealerHand endp
 
 addCardPlayer proc
-	mov eax, 12 ;get range for RandomRange 
+	mov eax, 13 ;get range for RandomRange 
 	call RandomRange
 	add eax, 2 ;get range 2-14
 	movzx ebx, cardsInPlayerHand ;counter for cards in hand
 	mov [playerHand + ebx], al ;create new value in next empty space in playerHand
 	inc ebx
-	mov cardsInPlayerHand, bl ;update card counter 
+	mov cardsInPlayerHand, bl ;update card counter
+
+	cmp eax, 10 ;compare to determine if facecard
+	jg faceCard
+
+	;numbered cards
+	mov ebx, sumOfPlayerHand ;copy sum to ebx
+	add eax, ebx ;add new card to sum
+	mov sumOfPlayerHand, eax ;move new sum to memory
 	ret
+
+	faceCard:
+		mov ebx, sumOfPlayerHand
+		add ebx, 10
+		mov sumOfPlayerHand, ebx
+		ret
+
 addCardPlayer endp
 
 addCardDealer proc
@@ -431,18 +463,28 @@ addCardDealer proc
 	movzx ebx, cardsInDealerHand ;counter for cards in hand
 	mov [dealerHand + ebx], al ;create new value in next empty space in dealer hand
 	inc ebx
-	mov cardsInDealerHand, bl ;update card counter 
+	mov cardsInDealerHand, bl ;update card counter
+
+	cmp eax, 10 ;compare to determine if facecard
+	jg faceCard
+
+	;numbered cards
+	mov ebx, sumOfDealerHand ;copy sum to ebx
+	add eax, ebx ;add new card to sum
+	mov sumOfDealerHand, eax ;move new sum to memory
 	ret
+
+	faceCard:
+		mov ebx, sumOfDealerHand
+		add ebx, 10
+		mov sumOfDealerHand, ebx
+		ret
 addCardDealer endp
 
 dealing proc
-	mov edx, OFFSET dealerHandIndicator
-	call WriteString ;Title hand
 	call addCardDealer ;deal one card to dealer
 	call addCardDealer ;deal one card to dealer
 	call displayDealerHand ;display dealer hand
-	mov edx, OFFSET playerHandIndicator
-	call WriteString ;Title hand
 	call addCardPlayer ;deal one card to player
 	call addCardPlayer ;deal one card to player
 	call displayPlayerHand ;show player their hand
@@ -456,22 +498,19 @@ getUserInput proc ;NEEDS WORK
 	mov ecx, SIZEOF input ;load max character parameter
 	call ReadString ; call Irvine procedure user input stored in 'input'
 
-	mov edx, OFFSET input
-	mov ebx, OFFSET hitTest
-	call Str_compare
-	jnc stay
-	mov eax, 4
+	cmp eax, 3 ;eax contains length of input string compare with 3(hit)
+	je hit
 
-	call addCardPlayer
-	call displayPlayerHand
+	;stay
+	;call addCardDealer ;player stays so give dealer card and display hand
+	;call displayDealerHand
+	mov stayValue, 1
+	ret
 
-	stay:
-		call addCardDealer
-		call displayDealerHand
+	hit:;hit
+		call addCardPlayer ;player hits so give them card and display hand
+		call displayPlayerHand
 		ret
-
-
-
 	
 getUserInput endp
 	
@@ -479,8 +518,39 @@ getUserInput endp
 main proc
 	call Randomize ;seed RNG for program
 	call dealing ;deal and display 2 cards to player and dealer "dummy card" for dealer displayed upside down
+	beginwhile:
+		cmp sumOfPlayerHand, 21 ;check if player has 21
+		jge checkWin 
+		cmp stayValue, 1 ;find if player stays
+		je dealerTurn ;allow dealer to go
+		call getUserInput
+		jmp beginwhile ;continue loop
 
+		dealerTurn: ;alloow the dealer to go
+			mov eax, sumOfPlayerHand
+			cmp sumOfDealerHand, eax
+			jg Lose
+			call addCardDealer
+			call displayDealerHand
+			cmp sumOfDealerHand, 21
+			jg Win
+			je Lose
 
+	checkWin:
+		cmp sumOfPlayerHand, 21
+		jne Lose
+		je Win
+	Win:
+		mov edx, OFFSET winMessage
+		call WriteString
+		jmp endL
+
+	Lose:
+		mov edx, OFFSET loseMessage
+		call WriteString
+
+	endL:
+		mov eax, ebx
 	exit
 main endp
 end main
